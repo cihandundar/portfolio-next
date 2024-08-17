9eb72e2d92580611f367bb0c9bf26f9b139cd77a
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-08-17 22:22:28 +0300

    data created

---
+++ src/data/Project.js
@@ -0,0 +1,40 @@
+export const projects = [
+  {
+    id: 1,
+    name: "E-Commerce Website",
+    image: "/shopping.jpg",
+    technologies: [
+      {
+        id: 1,
+        image: "/react.png",
+      },
+      {
+        id: 2,
+        image: "/scss.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Admin Dashboard",
+    image: "/admin.jpg",
+    technologies: [
+      {
+        id: 1,
+        image: "/react.png",
+      },
+      {
+        id: 2,
+        image: "/scss.png",
+      },
+      {
+        id: 3,
+        image: "/redux.png",
+      },
+      {
+        id: 4,
+        image: "/firebase.png",
+      },
+    ],
+  },
+];
