7b1e55c14601a70e4f9778041399e730de02ff44
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-08-18 01:14:47 +0300

    fix

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -15,12 +15,6 @@ const Hero = () => {
               style={{ fontWeight: "700" }}
               speed={50}
             />
-            <p>
-              "I'm a self-taught Frontend Developer who builds interfaces for
-              users to have great experiences,trying to improve myself on the
-              <br />
-              web since December 2021 and enjoying it!"
-            </p>
           </div>
         </div>
       </div>
