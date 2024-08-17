793a2358b1af264f3f8501439b15435038212c77
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-08-18 01:11:19 +0300

    fix

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -9,12 +9,18 @@ const Hero = () => {
           <img src="/photo.png" alt="photo" />
           <div className="logo__text">
             <span>Cihan Dündar</span>
-            <span className="tag"></span>
             <TypeAnimation
               sequence={["Junior Frontend Developer"]}
               wrapper="span"
+              style={{ fontWeight: "700" }}
               speed={50}
             />
+            <p>
+              "I'm a self-taught Frontend Developer who builds interfaces for
+              users to have great experiences,trying to improve myself on the
+              <br />
+              web since December 2021 and enjoying it!",
+            </p>
           </div>
         </div>
       </div>
