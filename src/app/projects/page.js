5111cbbaf130b11ca303112a3e80e4a7995f6876
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-08-17 23:37:56 +0300

    data created and mapped

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,10 +1,23 @@
 import React from "react";
-
+import { projects } from "../../data/Project";
 const Projects = () => {
   return (
     <section>
-      <div className="info">
+      <div>
         <h1>Projects</h1>
+        <div>
+          {projects.map((project) => (
+            <div className="project" key={project.id}>
+              <img src={project.image} alt={project.name} />
+              <h2>{project.name}</h2>
+              <div className="technologies">
+                {project.technologies.map((tech) => (
+                  <img src={tech.image} alt={tech.id} />
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
--- src/data/Project.js
+++ src/data/Project.js
@@ -1,7 +1,7 @@
 export const projects = [
   {
     id: 1,
-    name: "E-Commerce Website",
+    name: "E-Commerce Context API",
     image: "/shopping.jpg",
     technologies: [
       {
@@ -33,6 +33,78 @@ export const projects = [
       },
       {
         id: 4,
+        image: "/firebase.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Crypto Tracker",
+    image: "/kripto.jpg",
+    technologies: [
+      {
+        id: 1,
+        image: "/react.png",
+      },
+      {
+        id: 2,
+        image: "/scss.png",
+      },
+      {
+        id: 3,
+        image: "/redux.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "Vue - Shopping Cart",
+    image: "/vue-shopping.jpg",
+    technologies: [
+      {
+        id: 1,
+        image: "/vue.png",
+      },
+      {
+        id: 2,
+        image: "/scss.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "League of Legends ",
+    image: "/lol.jpg",
+    technologies: [
+      {
+        id: 1,
+        image: "/html.png",
+      },
+      {
+        id: 2,
+        image: "/css.png",
+      },
+      {
+        id: 3,
+        image: "/javascript.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "CRUD Operations",
+    image: "/blog.jpg",
+    technologies: [
+      {
+        id: 1,
+        image: "/react.png",
+      },
+      {
+        id: 2,
+        image: "/scss.png",
+      },
+      {
+        id: 3,
         image: "/firebase.png",
       },
     ],
