9e6394bd7252338c4d34179488fdfccec9196e90
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-08-18 01:05:17 +0300

    fix

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,10 +1,12 @@
+"use client";
 import "./projects.scss";
 import { projects } from "../../data/Project";
 import Card from "@/components/card/Card";
+import { TypeAnimation } from "react-type-animation";
 const Projects = () => {
   return (
     <section className="projects">
-      <span>Projects</span>
+      <TypeAnimation sequence={["PROJECTS"]} wrapper="span" speed={20} />
       <div className="projects__container">
         {projects.map((project) => (
           <Card
