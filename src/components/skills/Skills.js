ccf6df7b31f508ab0f544eae7ac75e5d781b1e04
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-08-18 01:42:13 +0300

    skills component added

---
+++ src/components/skills/Skills.js
@@ -0,0 +1,19 @@
+import "./skills.scss";
+import { skills } from "../../data/Skills";
+const Skills = () => {
+  return (
+    <section className="skills">
+      <span>Skills</span>
+      <div className="wrapper">
+        {skills.map((skill, index) => (
+          <div key={index} className="skills__item">
+            <img src={skill.image} alt="skillImage" />
+            <span>{skill.name}</span>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
